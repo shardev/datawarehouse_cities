224ac0cb1ec40b5cd053779b3b94e5052f13f480
Aleksandar <aleksandarsarovic@yahoo.com>
2024-04-25 16:30:12 +0200

    Added two int_country models, modified datadiagram

--- dbt_pg/models/intermediate/country/int_country__rank_per_life_expectancy.sql
+++ dbt_pg/models/intermediate/country/int_country__rank_per_life_expectancy.sql
@@ -1,19 +1,20 @@
--- todo
-select 
-	country_code,
-	country,
-	continent,
-	country_surface_area,
-	country_indep_year,
-	country_population,
-	country_life_expectancy,
-	case
-		when country_life_expectancy is not null then
-			dense_rank() over (order by country_life_expectancy desc) 
-		else null
-	end as country_life_exp_rank_in_world,
-	
-	country_gross_national_product,
-	country_government_form,
-	country_capital
-from
+with countries_life_exp_rank_in_world as (
+    select 
+        country_code,
+        country,
+        continent,
+        country_life_expectancy,
+        case
+            when country_life_expectancy is not null then
+                dense_rank() over (order by country_life_expectancy desc) 
+            else null
+        end as country_life_expectancy_rank_in_world,	
+        case
+            when country_life_expectancy is not null then
+                dense_rank() over (partition by continent order by country_life_expectancy desc) 
+            else null
+	    end as country_life_expectancy_rank_per_continent
+    from {{ ref('stg_world_db__countries') }}
+)
+
+select * from countries_life_exp_rank_in_world
